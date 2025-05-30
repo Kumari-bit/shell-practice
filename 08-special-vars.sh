d2e5008a08512a27e4cf305d09ad8f34a6b44b5e
Kumari <kumarivenkata994@gmail.com>
2025-05-30 18:16:56 +0530

    hello world created

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -4,7 +4,7 @@ echo "All variables passed to the script : $@"
 echo "Number of variables" : $#
 echo "Script Name : $0"
 echo "Current Directory : $PWD"
-echo "User running this script : $user"
+echo "User running this script : $USER"
 echo "Home Directory of user : $HOME"
 echo "PID of the script : $$"
 Sleep 10&

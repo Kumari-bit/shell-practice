9f328fb15a0d0fa855724982a611c6c724430c5e
Kumari <kumarivenkata994@gmail.com>
2025-07-12 20:53:52 +0530

    hello world started

--- 13-colours.sh
+++ 13-colours.sh
@@ -1,57 +1,45 @@
-#!/bin/bash
+#!/bash/bin
 
 USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-
+N="\e[34m"
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access"
-    exit 1 #give other than 0 upto 127
+    echo -e "$R ERROR: please run the script with root access$N"
+    exit 1
 else
-    echo "You are running with root access"
+    echo "your running the script with root access"
 fi
 
-# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo "Installing $2 is ... SUCCESS"
+        echo -e "Installing $2 is $G success $N"
     else
-        echo "Installing $2 is ... FAILURE"
-        exit 1
+        echo "Installing $2 is $R failure $N"
     fi
 }
 
 dnf list installed mysql
-if [ $? -ne 0 ]
-then
-    echo "MySQL is not installed... going to install it"
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
-else
-    echo "MySQL is already installed...Nothing to do"
-fi
+    if [ $? -ne 0 ]
+    then
+        echo "mysql is not installed...going to install"
+        dnf install mysql -y
+        VALIDATE $? mysql
+    else
+        echo "mysql is already installed nothing to do"
+        fi
 
 dnf list installed python3
-if [ $? -ne 0 ]
-then
-    echo "python3 is not installed... going to install it"
-    dnf install python3 -y
-    VALIDATE $? "python3"
-else
-    echo "python3 is already installed...Nothing to do"
-fi
-
-dnf list installed nginx
-if [ $? -ne 0 ]
-then
-    echo "nginx is not installed... going to install it"
-    dnf install nginx -y
-    VALIDATE $? "nginx"
-else
-    echo "nginx is already installed...Nothing to do"
-fi
+    if [ $? -ne 0 ]
+    then
+        echo "Python3 is not installed....going to install"
+        dnf install python3 -y
+        VALIDATE $? python3
+    else
+        echo "Python3 already install... nothing to do"
+        fi

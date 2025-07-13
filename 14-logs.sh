4885fdb4e9a7a0398463e40be4a63a7058f2d6e4
Kumari <kumarivenkata994@gmail.com>
2025-07-13 10:50:04 +0530

    hello world started

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,19 +5,20 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto 127
 else
-    echo "You are running with root access" | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a $Log_FILE
 fi
 
 # validate functions takes input as exit status, what command they tried to install
@@ -35,7 +36,7 @@ dnf list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed... going to install it" | tee -a $LOG_FILE
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y &>>LOG_FILE
     VALIDATE $? "MySQL"
 else
     echo -e "Nothing to do MySQL... $Y already installed $N" | tee -a $LOG_FILE

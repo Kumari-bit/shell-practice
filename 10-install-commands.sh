0a5d126af2b5984d487d9aee366efd36626864e5
Kumari <kumarivenkata994@gmail.com>
2025-08-20 16:31:09 +0530

    hello world

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
+USER=(id -u)
+if [ $USER -ne 0 ]
 then
-    echo "ERROR::Please run this script with root access"
-else
-    echo "Your running with root access"
+    echo "ERORR:please run the script with root access"
+    exit 1
 fi

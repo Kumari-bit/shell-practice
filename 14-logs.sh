52e918c1935dba6e0de8277bf99f4514b6f99010
Kumari <kumarivenkata994@gmail.com>
2025-08-24 17:15:37 +0530

    hello world

--- 14-logs.sh
+++ 14-logs.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"

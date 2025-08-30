a048dd50ce6cd3450ee41864abc2aa46cac8803f
Kumari <kumarivenkata994@gmail.com>
2025-08-30 14:03:09 +0530

    hello world

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -29,3 +29,40 @@ else
     echo "Mysql is already install nothing to do"
 fi
 
+echo "Starting nginx installation..."
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo "Nginx is not installed... going to install it"
+    dnf install nginx-server -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing nginx is success"
+    else
+        echo "Installing nginx is failure"
+        exit 1
+    fi
+else
+    echo "nginx is already install nothing to do"
+fi
+
+echo "Starting Python3 installation..."
+
+dnf list installed Python3
+
+if [ $? -ne 0 ]
+then
+    echo "Python3 is not installed... going to install it"
+    dnf install python3-server -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing python3 is success"
+    else
+        echo "Installing python3 is failure"
+        exit 1
+    fi
+else
+    echo "Python3 is already install nothing to do"
+fi

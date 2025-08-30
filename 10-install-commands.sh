a3174365b414494236d06720faa14abac1fec183
Kumari <kumarivenkata994@gmail.com>
2025-08-30 13:04:56 +0530

    hello world

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -5,26 +5,10 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then
     echo "ERROR: Please run the script with root access"
-    exit 1
+ 
 else
     echo "User is running with root access"
 fi
 
 dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
-    echo "Mysql is not installed.....going to install it"
-    dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "Mysql installing....success"
-    else
-        echo "Mysql installing....failure"
-    exit 1
-fi
-else
-    echo "Mysql is already installed nothing to do"
-fi
-
 

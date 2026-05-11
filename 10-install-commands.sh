5657e384f8e45dfe8581cbbed6814fede58e7340
Kumari <kumarivenkata994@gmail.com>
2026-05-11 17:51:30 +0530

    hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-USER=$(id -u)
+USERID=$(id -u)
 
-if [ $USER -ne 0 ]
-then 
-	echo "ERROR: Please run the script with root access"
-	exit 1
-else
-	echo "your running with root access
-fi
-
-dnf installed mysql -y
-if [ $? -ne 0]
+if [ $USERID -ne 0 ]
 then
-	echo "Myql is not installed....going to install it"
-	dnf install mysql -y
-	if [ $? -eq 0 ]
-	then
-		echo "Installing mysql is.....success"
-	else
-		echo "installing mysql is...failure"
-		exit 1
-	fi
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
 else
-	echo "Mysql is already install..Nothing to do"
+    echo "You are running with root access"
 fi
-	
+
+dnf list installed mysql
+
+# check already installed or not. if Installed $? is 0, then 
+# If not installed $? is not 0. expression is true
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is ... SUCCESS"
+    else
+        echo "Installing MySQL is ... FAILURE"
+        exit 1
+    fi
+else
+    echo "MySQL is already installed...Nothing to do"
+fi
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then
+#     echo "Installing MySQL is ... SUCCESS"
+# else
+#     echo "Installing MySQL is ... FAILURE"
+#     exit 1
+# fi

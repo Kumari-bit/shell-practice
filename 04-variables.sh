b4fea7df9c71f92bb2fb8fe20d9302019b9071b2
Kumari <kumarivenkata994@gmail.com>
2026-05-11 13:43:20 +0530

    hello world started

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,9 +3,10 @@
 PERSON1=$1
 PERSON2=$2
 PERSON3=$3
+PERSON4=$4
 
 echo "$PERSON1:: Hey $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
-echo "$PERSON1: I am fine too.what's up?"
+echo "$PERSON4: I am fine too.what's up?"
 echo "$PERSON3:: Nothing, just going to Mars now, will you come?"
 echo "$PERSON1: Sorry, you carry on! I will come once you come back"
